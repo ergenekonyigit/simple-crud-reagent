9351579aa41205ce823057729612122fa9892413
Ergenekon Yigit <yigitergenekon@gmail.com>
2019-03-19 14:18:25 +0300

    fix

--- src/simple_crud/db.cljs
+++ src/simple_crud/db.cljs
@@ -5,7 +5,6 @@
 (defonce app-state (r/atom {:users [{:id (uuid) :name "Caitlyn Kerluke" :username "caitlyn.kerluke"}
                                     {:id (uuid) :name "Rowan Nikolaus" :username "rowan.nikolaus"}
                                     {:id (uuid) :name "Elton Douglas" :username "elton.douglas"}
-                                    {:id (uuid) :name "Christophe Bartell" :username "christophe.bartell"}
                                     {:id (uuid) :name "Sam Pull" :username "sam.pull"}
                                     {:id (uuid) :name "Carson O. Gin" :username "carson.o.gin"}]
                             :editing false
